dbab56aabb858214f6405ffec0ade0982c21cdea
Sashkoe <revolver.bmx@gmail.com>
2024-10-20 14:02:36 +0200

    Update sendEmail.js

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,7 +4,6 @@ import env from './env.js';
 const nodemailerConfig = {
   host: env('SMTP_HOST'),
   port: env('SMTP_PORT'),
-  secure: true,
   auth: {
     user: env('SMTP_USERNAME'),
     pass: env('SMTP_PASSWORD'),
